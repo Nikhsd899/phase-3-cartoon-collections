b7205928a7a66a9b4bbd255f4ea3a1b81b1f4085
Nicholas <nikhsd899@outlook.com>
2022-01-18 17:46:44 -0800

    Completed Assignment

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -8,24 +8,24 @@ dwarves = %w[
   Happy
 ]
 
-
 def roll_call_dwarves(dwarves)
   dwarves.each_with_index do |dwarf, index|
     puts "#{index + 1}. #{dwarf}"
-  end 
+  end
 end
 
 roll_call_dwarves(dwarves)
 
+
 planeteer_calls = %w[earth wind fire water heart]
 
-def summon_captain_planet(planeteer_calls) # code an argument here
+def summon_captain_planet(planeteer_calls)
   planeteer_calls.map { |call| call.capitalize + '!' }
 end
 
 summon_captain_planet(planeteer_calls)
 
-def long_planeteer_calls(planeteer_calls)# code an argument here
+def long_planeteer_calls(planeteer_calls)
   planeteer_calls.any? { |call| call.length > 4 }
 end
 
@@ -34,10 +34,11 @@ long_planeteer_calls(planeteer_calls)
 potentially_cheesy_items = %w[umbrella spinach cheddar helicopter]
 
 def find_the_cheese(potentially_cheesy_items)
-  cheeses = %w[cheddar gouda camembert]
+  cheeses = %w[gouda cheddar camembert]
 
   potentially_cheesy_items.find do |maybe_cheese|
     cheeses.include?(maybe_cheese)
+
   end
 end
 
